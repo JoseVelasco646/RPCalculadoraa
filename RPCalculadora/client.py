bfe45d86c87fa4b7bfdabade27ba5941d66f7756
Jose Velasco <al19760627@ite.edu.mx>
2024-10-08 23:22:13 -0700

    calculadora v2

--- RPCalculadora/client.py
+++ RPCalculadora/client.py
@@ -1,15 +1,40 @@
 import xmlrpc.client
 
-proxy = xmlrpc.client.ServerProxy("http://localhost:9000/")
-result = proxy.add(2, 3)
-print("Result of 2 + 3 is:", result)
+def main():
+    proxy = xmlrpc.client.ServerProxy("http://localhost:9000/")
+    
+    while True:
+        print("Elige una operación:")
+        print("1: Sumar")
+        print("2: Restar")
+        print("3: Multiplicar")
+        print("4: Dividir")
+        print("5: Salir")
+        
+        opc = input("Ingresa tu elección (1/2/3/4/5): ")
+        
+        if opc == '5':
+            print("Saliendo...")
+            break
+        
+        if opc in ['1', '2', '3', '4']:
+            a = float(input("Ingresa el primer número: "))
+            b = float(input("Ingresa el segundo número: "))
+            
+            if opc == '1':
+                result = proxy.add(a, b)
+                print("Resultado de {} + {} es: {}".format(a, b, result))
+            elif opc == '2':
+                result = proxy.rest(a, b)
+                print("Resultado de {} - {} es: {}".format(a, b, result))
+            elif opc == '3':
+                result = proxy.multi(a, b)
+                print("Resultado de {} * {} es: {}".format(a, b, result))
+            elif opc == '4':
+                result = proxy.div(a, b)
+                print("Resultado de {} / {} es: {}".format(a, b, result))
+        else:
+            print("Elección no válida. Intenta de nuevo.")
 
-result = proxy.rest(2, 3)
-print("Result of 2 - 3 is:", result)
-
-result = proxy.multi(2, 3)
-print("Result of 2 * 3 is:", result)
-
-result = proxy.div(2, 10)
-print("Result of 2 / 3 is:", result)
- 
+if __name__ == "__main__":
+    main()
--- RPCalculadora/server.py
+++ RPCalculadora/server.py
@@ -3,19 +3,16 @@ from xmlrpc.server import SimpleXMLRPCServer
 def add_numbers(a, b):
     return a + b
 
+def rest_numbers(a, b):
+    return a - b
 
-def rest_numbers(a,b):
-    return a -b
-
-def mult_numbers(a,b):
-    return a  * b
+def mult_numbers(a, b):
+    return a * b
 
 def div_numbers(a, b):
-        if b == 0:
-            return "Error al dividir "
-        return a / b
-
-
+    if b == 0:
+        return "Error al dividir por cero"
+    return a / b
 
 server = SimpleXMLRPCServer(('localhost', 9000))
 print("Listening on port 9000...")
@@ -23,7 +20,5 @@ server.register_function(add_numbers, 'add')
 server.register_function(rest_numbers, 'rest')
 server.register_function(mult_numbers, 'multi')
 server.register_function(div_numbers, 'div')
-
-
 
 server.serve_forever()
